749380f89a4bef7d83c540a24577e74828dcb20f
Park minyeong <140313197+meanyong@users.noreply.github.com>
2025-08-18 16:22:06 +0900

    Update and rename Main_19539_사과나무.java to Main_19539_사과나무_박민영.java

--- 2주차/Main_19539_사과나무_박민영.java
+++ 2주차/Main_19539_사과나무_박민영.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_19539_사과나무 {
+public class Main_19539_사과나무_박민영 {
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
